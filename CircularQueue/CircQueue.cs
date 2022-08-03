1ed06465b05d40c3b1d256b7742d296e657ad957
unknown <erfanffa2020@gmail.com>
2022-08-03 13:38:57 +0430

    Adding Files

---
+++ CircularQueue/CircQueue.cs
@@ -0,0 +1,93 @@
+﻿namespace System
+{
+    public class CircQueue
+    {
+        int rear = -1;
+        int front = -1;
+        int[] array;
+
+        public CircQueue(int size)
+        {
+            array = new int[size];
+        }
+
+        public bool IsEmpty()
+        {
+            if (front == -1)
+                return true;
+            return false;
+        }
+
+        public bool IsFull()
+        {
+            if ((front == 0 && rear == array.Length - 1) || front == rear + 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Enqueue(int value)
+        {
+            if (!IsFull())
+            {
+                if (front == -1)
+                {
+                    front = 0;
+                }
+                rear = (rear + 1) % array.Length;
+                array[rear] = value;
+
+            }
+            else
+            {
+                Console.WriteLine("queue is full");
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                Console.WriteLine(array[front]);
+                if (rear == front)
+                {
+                    rear = -1;
+                    front = -1;
+                }
+                else
+                {
+                    front = (front + 1) % array.Length;
+                }
+            }
+            else
+            {
+                Console.WriteLine("queue is empty");
+            }
+        }
+
+        public void PrintMember()
+        {
+            if (!IsEmpty())
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    if (front <= rear)
+                    {
+                        int item = array[i];
+                        Console.WriteLine("--" + item);
+                    }
+                    else if (rear >= array.Length - 1)
+                    {
+                        int item = (rear + 1) % array.Length;
+                        Console.WriteLine("--" + array[item]);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("queue is empty");
+            }
+        }
+    }
+}
